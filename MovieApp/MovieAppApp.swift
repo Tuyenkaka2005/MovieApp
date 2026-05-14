9947f89b98cafdf1a351ede83a90eebfd076b32d
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-13 20:14:28 -0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Tuyen on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
